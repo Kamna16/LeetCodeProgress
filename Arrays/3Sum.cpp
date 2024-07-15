eacb73147313a4e02d67641254980604c15751a5
kamna <kamnabhadoriya042@gmail.com>
2024-07-16 00:17:55 +0530

    3Sum

---
+++ Arrays/3Sum.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1]) ++j; 
+                    while (j < k && nums[k] == nums[k - 1]) --k; 
+                    ++j;
+                    --k;
+                } else if (sum < 0) {
+                    ++j;
+                } else {
+                    --k;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
