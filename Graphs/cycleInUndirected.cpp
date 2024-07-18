58c139110bfe2363e5993e68944521d499e86e7e
kamna <kamnabhadoriya042@gmail.com>
2024-07-18 22:05:00 +0530

    cycle in undirected graph

--- Graphs/cycleInUndirected.cpp
+++ Graphs/cycleInUndirected.cpp
@@ -24,3 +24,48 @@ class Solution {
         return false;
     }
 };
+
+// using bfs
+class Solution {
+    private:
+    bool detect(int src, vector<int> adj[], int vis[])
+    {
+        vis[src] = 1;
+        queue<pair<int, int>> q;
+        q.push({src,-1});
+        
+        while(!q.empty())
+        {
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            
+            for(auto neighbour : adj[node])
+            {
+                if(!vis[neighbour])
+                {
+                    vis[neighbour] =1;
+                    q.push({neighbour, node});
+                }
+                else if(parent != neighbour)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+  public:
+    bool isCycle(int V, vector<int> adj[]) {
+       int vis[V] = {0};
+       // considering multiple components
+       for(int i=0; i<V; i++)
+       {
+           if(!vis[i])
+           {
+               if(detect(i,adj,vis)) return true;
+           }
+       }
+       return false;
+    }
+};
