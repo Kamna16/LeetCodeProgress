d03f4b905381c08f294736cfbffb8c10a7c12c51
kamna <kamnabhadoriya042@gmail.com>
2024-07-20 19:51:00 +0530

    anagrams

---
+++ Strings/groupAnagrams.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+        
+        for (string& str : strs) {
+            string sortedStr = str;
+            sort(sortedStr.begin(), sortedStr.end());
+            anagramGroups[sortedStr].push_back(str);
+        }
+        
+        vector<vector<string>> result;
+        for (auto& group : anagramGroups) {
+            result.push_back(group.second);
+        }
+        
+        return result;
+    }
+};
