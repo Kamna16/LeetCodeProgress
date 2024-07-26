74d588db138a969c9f7370205fc4f66cd3c0b20f
kamna <kamnabhadoriya042@gmail.com>
2024-07-26 14:17:05 +0530

    floyd warshalla Algo

---
+++ Graphs/floydWarshall.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    int n = matrix.size();
+	   for(int i=0;i<n;i++)
+	   {
+	       for(int j=0;j<n;j++)
+	       {
+	           if(matrix[i][j] == -1) matrix[i][j] = 100000;
+	       }
+	   }
+	   for(int via=0;via<n;via++)
+	   {
+	       for(int i=0;i<n;i++)
+    	   {
+    	       for(int j=0;j<n;j++)
+    	       {
+    	           matrix[i][j] = min(matrix[i][j], matrix[i][via] + matrix[via][j]);
+    	       }
+    	   }
+	   }
+	   for(int i=0;i<n;i++)
+	   {
+	       for(int j=0;j<n;j++)
+	       {
+	           if(matrix[i][j] == 100000) matrix[i][j] = -1;
+	       }
+	   }
+	}
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>matrix(n, vector<int>(n, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution obj;
+		obj.shortest_distance(matrix);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cout << matrix[i][j] << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
