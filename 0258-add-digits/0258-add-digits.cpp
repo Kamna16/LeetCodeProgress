aca005440c4f4a9b5a716821b2d472eb7d83f89b
Kamna Bhadoriya <97112916+Kamna16@users.noreply.github.com>
2024-08-03 10:16:32 +0530

    Time: 2 ms (38.94%), Space: 8.3 MB (24.16%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while( num > 9)
-        {
-            int sum = 0;
-            while(num > 0){
-                sum  += num% 10;
-                num = num/10;
-            }
-            num = sum;
-        }
-        return num;
+        if(num == 0) return 0;
+        return num%9 == 0 ? 9 : num%9;
     }
 };
