686b6dad3c63b807e0a7821f7e815bf57f144a6d
Kamna Bhadoriya <97112916+Kamna16@users.noreply.github.com>
2024-08-06 11:52:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (18.45%) - LeetHub

--- 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n = word.size();
-        unordered_map<int,int> mp;
-        int sum =0;
-        int assign =2;
+        vector<int> mp(26,0);
         for(char ch : word)
         {
-            if(assign > 9) assign =2;
-
-            mp[assign]++;
-            sum+= mp[assign];
-            assign++;
+            mp[ch-'a']++;
+        }
+        sort(mp.begin(),mp.end(),greater<int>());
+        int sum =0;
+        for(int i=0;i<26;i++)
+        {
+            int freq = mp[i];
+            int press = i/8+1;
+            sum+= press * freq;
         }
         return sum;
     }
