be73568cff2143bb0719ba4abff8282a27e05cc2
Kamna Bhadoriya <97112916+Kamna16@users.noreply.github.com>
2024-09-12 22:04:55 +0530

    Time: 18 ms (14.12%), Space: 25.9 MB (10.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    vector<int> getLeftMax(vector<int>& height, int n) {
-        vector<int> leftMax(n);
-        leftMax[0] = height[0];
-        for(int i = 1; i<n; i++) {
-            leftMax[i] = max(leftMax[i-1], height[i]);
+    vector<int> calcLeft(vector<int>& height)
+    {
+        vector<int> ans(height.size());
+        ans[0] = height[0];
+        for(int i=1;i<height.size();i++)
+        {
+            ans[i] = max(ans[i-1], height[i]);
         }
-        return leftMax;
+        return ans;
     }
-    vector<int> getRightMax(vector<int>& height, int n) {
-        vector<int> rightMax(n);
-        rightMax[n-1] = height[n-1];
-        for(int i = n-2; i>=0; i--) {
-            rightMax[i] = max(rightMax[i+1], height[i]);
-        }
-        return rightMax;
-    } 
-    int trap(vector<int>& height) {
+    vector<int> calcRight(vector<int>& height)
+    {
         int n = height.size();
-        if(n == 1 || n == 0)
-            return 0;
-        vector<int> leftMax = getLeftMax(height, n);
-        vector<int> rightMax = getRightMax(height, n);
-        int sum = 0;
-        for(int i = 0; i<n; i++) {
-            sum += min(leftMax[i], rightMax[i]) - height[i];
+        vector<int> ans(n);
+        ans[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            ans[i] = max(ans[i+1], height[i]);
         }
-        return sum;
+        return ans;
+    }
+    int trap(vector<int>& height) {
+        int count =0;
+        int n = height.size();
+        vector<int> leftMax = calcLeft(height);
+        vector<int> rightMax = calcRight(height);
+        for(int i=0;i<n;i++)
+        {
+            count += min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return count;
     }
 };
