77b2c43243f9b3e931de54cdeb3907518d763775
Kamna Bhadoriya <97112916+Kamna16@users.noreply.github.com>
2024-09-17 21:10:48 +0530

    Time: 3 ms (66.72%), Space: 7.6 MB (12.8%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0) return 0;
+        if(n==1) return 1;
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
