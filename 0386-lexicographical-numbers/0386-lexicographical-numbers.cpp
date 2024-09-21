5199ea94ef97aab9f1c7946a5c311c262ab50ca5
Kamna Bhadoriya <97112916+Kamna16@users.noreply.github.com>
2024-09-21 19:09:38 +0530

    Time: 16 ms (55.05%), Space: 13.8 MB (69.49%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+    void solve(int n, vector<int> &ans, int curr)
+    {
+        if(curr > n ) return;
+        ans.push_back(curr);
+        for(int i=0;i<=9;i++)
+        {
+            int currNum = curr*10 + i;
+            solve(n,ans,currNum);
+        }
+    }
     vector<int> lexicalOrder(int n) {
         vector<int> ans;
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=9;i++)
         {
-            ans.push_back(i);
+            solve(n,ans,i);
         }
-        auto myComparator = [](int a, int b)
-        {
-            string s1 = to_string(a);
-            string s2 = to_string(b);
-            return s1<s2;
-        };
-        sort(ans.begin(),ans.end(),myComparator);
         return ans;
     }
 };
